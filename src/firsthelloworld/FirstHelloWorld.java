353bfc068b3752cf071336cb9d120dd6e64f8db4
g15919km <g15919km@s30447.ACTSUDA>
2017-04-14 11:51:15 +0900

    日本語を追加しました

--- src/firsthelloworld/FirstHelloWorld.java
+++ src/firsthelloworld/FirstHelloWorld.java
@@ -17,6 +17,7 @@ public class FirstHelloWorld {
     
     public static void main(String[] args) {
         System.out.println("Hello,World!");
+        System.out.println("こんにちは、世界！");
     }
     
 }
